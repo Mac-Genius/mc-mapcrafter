b3c3db67674820dd0d63b4100c5546719cb01717
Mac-Genius <jwh0051@auburn.edu>
2018-03-28 19:11:54 -0500

    Add google material icons

--- Dockerfile
+++ Dockerfile
@@ -2,9 +2,13 @@ FROM ubuntu
 
 WORKDIR /tmp
 
+# Create directories
+RUN mkdir /data && \
+	mkdir /data/worlds
+
 # Install mapcrafter
 RUN apt-get update && \
-	apt-get install -y lsb-core wget apt-transport-https && \
+	apt-get install -y lsb-core wget apt-transport-https git && \
 	ln -s /usr/bin/python3 /usr/bin/python && \
 	echo "deb http://packages.mapcrafter.org/ubuntu $(lsb_release -sc) main" | tee /etc/apt/sources.list.d/mapcrafter.list && \
 	wget -O /etc/apt/trusted.gpg.d/mapcrafter.gpg http://packages.mapcrafter.org/ubuntu/keyring.gpg && \
@@ -13,14 +17,15 @@ RUN apt-get update && \
 
 # Setup directories
 
-RUN mkdir /data && \
-	mkdir /data/worlds
-
 WORKDIR /data
+
+RUN git clone https://github.com/google/material-design-icons.git icons
 
 COPY files/entrypoint.sh /data/entrypoint.sh
 COPY files/mapcrafter-template.conf /data/mapcrafter-template.conf
 
 ENV CONFIG=mapcrafter.conf
+ENV MAP="true"
+ENV ICONS="false"
 
 ENTRYPOINT [ "bash", "entrypoint.sh" ]
--- files/entrypoint.sh
+++ files/entrypoint.sh
@@ -6,8 +6,28 @@ if [ ! -f /data/config/mapcrafter.conf ]; then
 	cp /data/mapcrafter-template.conf /data/config/mapcrafter.conf
 fi
 
-if [ -d /data/worlds/world ]; then
+if [ -d /data/worlds/world -a "$MAP" = "true" ]; then
 	mapcrafter -c /data/config/$CONFIG
+else
+	printf "World folder does not exist!"
+fi
+
+if [ -d /data/worlds/world -a "$ICONS" = "true" ]; then
+	printf "Placing markers...\n"
+	mapcrafter_markers -c /data/config/$CONFIG
+	if [ ! -d /data/output/static/markers/action ]; then
+		mkdir /data/output/static/markers/action
+	fi
+	if [ ! -d /data/output/static/markers/maps ]; then
+		mkdir /data/output/static/markers/maps
+	fi
+	if [ ! -d /data/output/static/markers/image ]; then
+		mkdir /data/output/static/markers/image
+	fi
+	cp -r /data/icons/action/2x_web/*48dp.png /data/output/static/markers/action/
+	cp -r /data/icons/maps/2x_web/*48dp.png /data/output/static/markers/maps/
+	cp -r /data/icons/image/2x_web/*48dp.png /data/output/static/markers/image/
+	printf "Complete.\n"
 else
 	printf "World folder does not exist!"
 fi
